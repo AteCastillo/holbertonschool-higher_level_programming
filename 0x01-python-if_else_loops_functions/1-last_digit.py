c40fc88461febdef3a16429eb121b166e045faf2
Atenea <atecast@gmail.com>
2020-12-08 16:28:36 +0000

    first adds

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastdi = (number * -1) % 10
+    lastdi = (lastdi * -1)
+else:
+    lastdi = number % 10
+
+if lastdi > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, lastdi))
+
+elif lastdi == 0:
+    print("Last digit of {} is {} and is 0".format(number, lastdi))
+
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, lastdi))
