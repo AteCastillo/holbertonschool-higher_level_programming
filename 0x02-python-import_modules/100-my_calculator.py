24a67a52f20617b04cdeba3df4ae8fdb068546f4
Atenea <atecast@gmail.com>
2020-12-10 20:54:30 +0000

    fixing file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     c = sys.argv[2]
 
     if c == '+':
-        print('{} + {} = {}'.format(a, b, add(a, b)))
+        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     elif c == '-':
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
+        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     elif c == '*':
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
+        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
     elif c == '/':
-        print('{} / {} = {}'.format(a, b, div(a, b)))
+        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
