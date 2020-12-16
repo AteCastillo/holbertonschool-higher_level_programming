92885cf23877cf9b92aed7db7851f684eb580e53
Atenea <atecast@gmail.com>
2020-12-16 02:29:30 +0000

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
+        length = 0
         sentence[0] = None
-        return 0
     else:
         length = len(sentence)
         first = sentence[0]
