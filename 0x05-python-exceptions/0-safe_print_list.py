0833d138d993f5787921ec5c43afe3139e75456a
Atenea <atecast@gmail.com>
2021-01-04 17:52:26 +0000

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
-        
         try:
-            count += 1
             print(my_list[i], end='')
-        except:
+            count += 1
+        except IndexError:
             break
     print()
     return count
