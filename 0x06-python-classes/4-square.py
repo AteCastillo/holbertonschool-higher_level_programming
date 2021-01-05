07756fa4c20077d5cb863351badce8e1a4a0b7bd
Atenea <atecast@gmail.com>
2021-01-05 20:51:48 +0000

    more adds

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,10 +8,23 @@ class Square:
         """Function to instantiation with size"""
         self.__size = size
         if type(size) is not int:
-            raise TypeError("size must be an integer")
+          raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+          raise ValueError("size must be >= 0")
 
     def area(self):
         """Returns the area of a square"""
         return self.__size ** 2
+
+    @property
+    def size(self):
+        """to retrieve size"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """to change size"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
