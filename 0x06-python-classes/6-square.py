885fe7375df8792ca86def22dd7b198b7c096311
Atenea <atecast@gmail.com>
2021-01-06 00:41:01 +0000

    fix 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,11 +7,19 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Function to instantiation with size"""
         self.__size = size
-        self.__position = position
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+        self.__position = position
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         """Returns the area of a square"""
@@ -39,7 +47,11 @@ class Square:
     @position.setter
     def position(self, value):
         """to set position"""
-        if len(self.__position) != 2 or value < 0:
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
