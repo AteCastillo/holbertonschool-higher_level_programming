3e83f267f0ca321e429dd806dc1de56b55d12dc8
Atenea <atecast@gmail.com>
2021-01-09 21:29:04 +0000

    more adds

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,11 +9,17 @@ def text_indentation(text):
     """
     text must be a string
     """
-
     if type(text) is not str:
         raise TypeError("text must be a string")
     
-    for i in text:
-        if i == '.' or  i == '?' or i == ':':
-            i = '\n'
-    print(text)
+    new_list = list(text)
+    for i in range(len(new_list)):
+        if new_list[i] is ".":
+            new_list[i + 1] = '\n\n'
+        if new_list[i] is "?":
+            new_list[i + 1] = '\n\n'
+        if new_list[i] is ":":
+            new_list[i + 1] = '\n\n'
+    new_tab = ""
+    new_tab = new_tab.join(new_list)
+    print("{}".format(new_tab), end="")
