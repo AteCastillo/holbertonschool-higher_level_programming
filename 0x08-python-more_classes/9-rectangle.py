d8ea1d99aa7bd91ded8ec16647e07c0ffdaca756
Atenea <atecast@gmail.com>
2021-01-12 01:20:05 +0000

    fix 2

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -12,8 +12,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """ equisde equisde """
-        return cls(size, size)
+        return Rectangle(size, size)
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
