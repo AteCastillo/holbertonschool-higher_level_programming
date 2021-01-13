c534190c08b43f995793850a83ac7a17acd8e051
Atenea <atecast@gmail.com>
2021-01-13 00:09:16 +0000

    fix 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -3,7 +3,6 @@
 Rectangle class
 """
 
-
 class Rectangle:
     """what are these for? who knows"""
 
@@ -33,14 +32,6 @@ class Rectangle:
         """Initializes the rectangle"""
         self.height = height
         self.width = width
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         Rectangle.number_of_instances += 1
 
     def __del__(self):
