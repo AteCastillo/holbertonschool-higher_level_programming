9f7538300143992753cc9d59a70be8878a784e95
AteCastillo <atecast@gmail.com>
2021-03-01 22:00:45 +0000

    new adds

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- list all records with a certain condition
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- score average
+SELECT AVG(score) FROM second_table;
