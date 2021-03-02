f73ef3408914e7fdac713428898570f8107d31f9
AteCastillo <atecast@gmail.com>
2021-03-02 21:57:36 +0000

    new

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,5 @@
 -- lists all genres in a database
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows 
+FROM tv_genres  JOIN tv_show_genres  ON tv_show_genres.genre_id = tv_genres.id WHERE tv_show_genres.genre_id IS NOT NULL GROUP BY tv_show_genres.genre_id 
+ORDER BY number_of_shows DESC;
