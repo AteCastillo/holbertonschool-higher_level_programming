a0569f29e06298e1041007a5d105cc1f5a61a3f8
AteCastillo <atecast@gmail.com>
2021-03-23 15:43:28 -0300

    advanced

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,13 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 const dict = require('./101-data.js').dict;
 
 const newDict = {};
 for (const key in dict) {
   const val = dict[key];
   if (newDict[val] === undefined) {
-    newDict[val] = [key];
+    newDict[val] = [key]; 
   } else {
-    newDict[val].push(key);
+    newDict[val].push(key); 
   }
 }
 console.log(newDict);
