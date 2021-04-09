1c243a4436038b9eda9acaeba54996b904dcf447
Ate Castillo <atecast@gmail.com>
2021-04-09 16:04:45 +0000

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 '''to fetch an url'''
-import urllib3.request
+import urllib.request
 
-with urllib3.request.urlopen('https://intranet.hbtn.io/status') as response:
-   html = response.read()
-print(type(html))
-print(html)
-print(html.decode())
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+print("Body response: \n\t- type: {} \n\t- content: {} \n\t- utf8 content: {}"
+      .format(type(html), html, html.decode))
