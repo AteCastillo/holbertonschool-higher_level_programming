2ce23ac9d813d1b795beb157c171ed6d0c5f6687
AteCastillo <atecast@gmail.com>
2021-05-04 16:04:39 +0000

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], function (err, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
+const url = process.argv[2];
+request(url, function (err, response) {
+  console.log('code: ', response.statusCode);
 });
